097f42d59b99544bca07a7a9d5437cd94fb20613
Elena <elenanikolaevna999@yandex.ru>
2019-04-10 11:23:05 +0300

    Initial commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogicgames.superjumper.SuperJumper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Game";
+		config.height = 1080;
+		config.width = 1920;
+		config.fullscreen=true;
+		new LwjglApplication(new SuperJumper(), config);
+	}
+}
